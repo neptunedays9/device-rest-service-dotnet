cff054d8568118142baa1974af5ba59a22c223fb
neptunedays9 <62010439+neptunedays9@users.noreply.github.com>
2020-03-10 20:14:22 +1100

    Add files via upload

---
+++ DeviceApp/Db/Device.cs
@@ -0,0 +1,17 @@
+﻿namespace DeviceApp.api.lib.Db
+{
+    public class Device
+    {
+        public int Id { get; set; }
+        
+        public string Colour { get; set; }
+
+        public string Model { get; set; }
+        
+        public decimal Price { get; set; }
+        
+        public int Year { get; set; }
+        
+        public string CountryManufactured { get; set; }
+    }
+}
